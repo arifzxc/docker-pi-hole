bf86de612f859216fe121993f90a02805c5e1a8c
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-20 05:03:28 +0700

    Delete pi-hole.sh

--- pi-hole.sh
+++
@@ -1,9 +0,0 @@
-# Masuk sebagai superuser (root) terlebih dahulu, agar mempermudah proses installasi. 
-
-sudo apt install apache2 php php-xmlrpc php-mysql php-gd php-cli php-curl docker.io docker-compose -y
-
-docker-compose up -d
-
-docker run -d -p 9000:9000 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer
-
-
